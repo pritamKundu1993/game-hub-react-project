9fb7fbae45198ea374144765e84ca29cf1f0fb12
pritam kundu <riku.kundu.1993@gmail.com>
2024-12-25 19:07:04 +0530

    implement chakra ui and using components and make it responsive

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Grid, GridItem } from '@chakra-ui/react';
+
+function App() {
+    return (
+        <>
+            <Grid
+                templateAreas={{
+                    base: '"nav" "main"',
+                    lg: '"nav nav" "aside main"',
+                }}
+            >
+                <GridItem area="nav" bg={'coral'}>
+                    nav
+                </GridItem>
+                <GridItem area="aside" bg="gold" display={{ base: 'none', lg: 'block' }}>
+                    aside
+                </GridItem>
+                <GridItem area="main" bg="dodgerblue">
+                    main
+                </GridItem>
+            </Grid>
+        </>
+    );
+}
+
+export default App;
